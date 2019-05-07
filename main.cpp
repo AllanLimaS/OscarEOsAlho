c8d5d45c891f441780a14d226c5f6755f367820a
AllanLimaS <49033925+AllanLimaS@users.noreply.github.com>
2019-05-07 13:16:01 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,81 @@
+
 #include "print_jogo.h"
 #include <QCoreApplication>
 #include <iostream>
 #include <QApplication>
+#include <QKeyEvent>
+#include <QDebug>
 
 using namespace std;
+
+
+void keyPressEvent(int matriz[][13]){
+    QKeyEvent *event;
+
+    for(int i;i<13;i++){
+        for(int j;j<13;j++){
+
+            if(matriz[i][j]==2){
+                if (event->key() == Qt::Key_Left){
+                    qDebug() <<"tecla";
+                    matriz[i-1][j] = 2;
+                    matriz[i][j] = 0;
+                }
+                else if (event->key() == Qt::Key_Right){
+                    matriz[i+1][j] = 2;
+                    matriz[i][j] = 0;
+                }
+                else if (event->key() == Qt::Key_Up){
+                    matriz[i][j-1] = 2;
+                    matriz[i][j] = 0;
+                }
+                else if (event->key() == Qt::Key_Down){
+                    matriz[i][j+1] = 2;
+                    matriz[i][j] = 0;
+                }
+            }else{}
+        }
+    }
+
+}
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    int matriz [13][13]={};
+    int matriz[13][13];             // 0 = espaço branco, 1 == parede
+    paredes_mapa(matriz);
+    matriz[10][11]=2;               //personagem
 
     QGraphicsScene * Tela_game = new QGraphicsScene;
-
     QGraphicsView view(Tela_game);
+
+    QPixmap ricardo_png(QPixmap("C:\\Users\\allan\\OneDrive\\Área de Trabalho\\OscarEOsAlho-master\\ricardo.png"));
+    QPixmap parede_png(QPixmap("C:\\Users\\allan\\OneDrive\\Área de Trabalho\\OscarEOsAlho-master\\parede.png"));
+    QPixmap chao_png(QPixmap("C:\\Users\\allan\\OneDrive\\Área de Trabalho\\OscarEOsAlho-master\\chao.png"));
 
     for(int i = 0;i<13;i++){
         for(int j = 0; j<13;j++){
             switch(matriz[i][j]){
-            case 0:
-                //Tela_game->addLine(1,i,500,i,QPen(Qt::black, 100, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-                Tela_game->addLine(i*50,j*50,i*50+50,j*50+50,QPen(Qt::black, 100, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+            case 0: //espaço em branco
+                Tela_game->addRect(QRect(QPoint(i*40,j*40),QSize(40,40)),QPen(Qt::black,2,Qt::SolidLine,Qt::RoundCap,Qt::RoundJoin),QBrush(chao_png));
                 break;
-            case 1:
+            case 1: //parede
+                Tela_game->addRect(QRect(QPoint(i*40,j*40),QSize(40,40)),QPen(Qt::black,2,Qt::SolidLine,Qt::RoundCap,Qt::RoundJoin),QBrush(parede_png));
+                break;
+            case 2: //personagem
+                Tela_game->addRect(QRect(QPoint(i*40,j*40),QSize(40,40)),QPen(Qt::black,2,Qt::SolidLine,Qt::RoundCap,Qt::RoundJoin),QBrush(ricardo_png));
                 break;
             }
-
         }
     }
-
+    qDebug() <<"alo";
     view.setFixedSize(800,600);
     view.show();
-
     return a.exec();
+
 }
+
+
--- print_jogo.h
+++ print_jogo.h
@@ -3,7 +3,21 @@
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
 #include <QGraphicsView>
+#include <stdio.h>
 
 
 
 #endif // PRINT_JOGO_H
+
+int paredes_mapa(int matriz[][13]){
+    for(int i=0;i<13;i++){
+        for(int j=0;j<13;j++){
+            if(i==0||j==0||i==12||j==12){
+                matriz[i][j]=1;
+            }else{
+                matriz[i][j]=0;
+            }
+        }
+    }
+    return 0;
+}
